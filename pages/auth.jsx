bd7968ab01e2824cecd74cf126581c435e29b70a
Niloy Sikdar <niloysikdar30@gmail.com>
2021-12-27 15:45:30 +0530

    Feat: Added Head in auth page

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import { getProviders, signIn } from 'next-auth/react';
 
@@ -5,27 +6,39 @@ import SpotifyImage from '../assets/spotify.png';
 
 const Auth = ({ providers }) => {
   return (
-    <div className='min-h-screen flex flex-col justify-center items-center bg-[#2941ab] text-white text-center p-5'>
-      <Image src={SpotifyImage} alt='Spotify Image' width={200} height={200} />
-      <h2 className='text-[#1ed760] text-4xl font-bold mt-8'>
-        Login With Spotify Right Now!
-      </h2>
-      <p className='mt-2 mb-10 text-xl font-medium text-[#1ed760]'>
-        Control your Spotify Player and listen to your favourite music from
-        anywhere :)
-      </p>
-      {Object.values(providers).map((provider) => {
-        return (
-          <button
-            key={provider.id}
-            className='bg-[#1ed760] text-[#2941ab] text-xl font-semibold px-7 py-2 pb-3 rounded-full'
-            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
-          >
-            Login with {provider.name}
-          </button>
-        );
-      })}
-    </div>
+    <>
+      <Head>
+        <title>Spotifire</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className='min-h-screen flex flex-col justify-center items-center bg-[#2941ab] text-white text-center p-5'>
+        <Image
+          src={SpotifyImage}
+          alt='Spotify Image'
+          width={200}
+          height={200}
+        />
+        <h2 className='text-[#1ed760] text-4xl font-bold mt-8'>
+          Login With Spotify Right Now!
+        </h2>
+        <p className='mt-2 mb-10 text-xl font-medium text-[#1ed760]'>
+          Control your Spotify Player and listen to your favourite music from
+          anywhere :)
+        </p>
+        {Object.values(providers).map((provider) => {
+          return (
+            <button
+              key={provider.id}
+              className='bg-[#1ed760] text-[#2941ab] text-xl font-semibold px-7 py-2 pb-3 rounded-full'
+              onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+            >
+              Login with {provider.name}
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
